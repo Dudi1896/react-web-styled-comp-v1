b4ec76641e1f1ed6ef16da685d649138e13b4f06
Dudi1896 <denzelu181@gmail.com>
2022-12-17 00:49:33 -0500

    footer additions

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterSubscription>
+        
+
+
+        <FooterSubText></FooterSubText>
+      </FooterSubscription>
+    </FooterContainer>
+  );
+};
+
+export default FootFooterContainer;
