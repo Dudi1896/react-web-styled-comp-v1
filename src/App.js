f36976ac64292ac7913d3b40ee72ff1ea0a03140
Dudi1896 <denzelu181@gmail.com>
2022-12-21 02:34:32 -0500

    final fixes

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import React from "react";
 import GlobalStyles from "./GlobalStyles";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Navbar, Footer } from "./Components";
-import Home from "./Pages/HomePage/Home";
+import { Home, Services, Products, SignUp } from "./Pages/";
+import ScrollToTop from "./Hooks/ScrollToTop";
 
 function App() {
   return (
     <>
       <Router>
         <GlobalStyles />
+        <ScrollToTop />
         <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/sign-up" element={<SignUp />} />
         </Routes>
         <Footer />
       </Router>
